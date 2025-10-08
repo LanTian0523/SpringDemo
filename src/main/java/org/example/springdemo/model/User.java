38e62997ed60cbc41025fa7c9b758dd94cad6cc8
lantian <skybluebluede@gmail.com>
2025-10-08 14:21:54 +0800

    Init commit

---
+++ src/main/java/org/example/springdemo/model/User.java
@@ -0,0 +1,10 @@
+package org.example.springdemo.model;
+
+
+public class User {
+    private Integer id;
+    private String name;
+    private Short gender;
+    private Short age;
+    private String phone;
+}
